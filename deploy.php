487fabfaa9883a4f20a8a7d9c25a2745fc91b577
Blue Road Media <33237731+BlueRoadMedia@users.noreply.github.com>
2022-04-25 09:50:12 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -12,8 +12,8 @@ require 'recipe/symfony.php';
 
 // TODO Add example configuration about multiple hosts which share some common settings.
 
-// Repository to deploy. This repository will be cloned to a server when creating
-// a new release.
+// Repository to deploy. This repository will be cloned to a hosting server
+// server when creating a new release.
 set('repository', 'git@github.com:[USER]/[REPO].git');
 
 // This env config will be used for `bin/console dump-env` command ('deploy:dump-env' task).
@@ -27,24 +27,37 @@ set('symfony_env', 'prod');
 // https://lorisleiva.com/deploy-your-laravel-app-from-scratch/install-and-configure-deployer#hosts
 
 // The hosts section defines a deployment location. You can call it by the
-// server url address (e.g. 'example.com', 'staging.example.com' etc.) or any other
-// way you see fit (e.g. 'production', 'staging' etc.)
+// hosting server url address (e.g. 'example.com', 'staging.example.com' etc.) or 
+// any other way you see fit (e.g. 'production', 'staging' etc.)
 host('[EXAMPLE.COM]')
-    // In case the actual remote server's ssh connection address is different
+    // In case the actual hosting server's ssh connection address is different
     // than what is host defined as, a 'hostname' setting must be provided.
-    // This config will be used for the actual ssh connection destination.
-    // ->set('hostname', 'example.cloud.com')
-    // It can also be a remote/hosting server ip address.
+    // The 'hostname' will be used for the actual ssh connection destination.
+    // 1. Can be a hosting server url.
+    // ->set('hostname', '[EXAMPLE.EXAMPLE.COM]')
+    // 2. Or it can be a hosting server ip address.
     // ->set('hostname', '63.245.75.182')
     // Website user. Deployer uses this config for actual ssh connection to a
-    // remote/hosting server (e.g. website_user@example.com).
+    // hosting server (e.g. website_user@example.com).
     ->set('remote_user', '[WEBSITE_USER]')
-    // The path to the location on the server where a Symfony application should
-    // be deployed.
+    // Ssh connection to a hosting server.
+    // 1. It is a good practice to keep hosting server's ssh connection parameters
+    // out of 'deploy.php'file, as they can change depending on where the deploy
+    // is executed from. Only specify 'hostname' and 'remote_user' and for the
+    // rest use the ssh 'config' file. The default location is '~/.ssh/config'.
+    // ->set('config_file', '[PATH/TO/SSH/CONFIG/FILE]')
+    // 2. Or provide the path to a private key for ssh connection to a hosting
+    // server.
+    // ->set('identity_file', '[PATH/TO/PRIVATE/KEY]')
+    // The path to the location on a hosting server where a Symfony application
+    // should be deployed.
     ->set('deploy_path', '[PATH/TO/APP/DIRECTORY]')
     // Deployer uses this config to assign writing permissions required by Symfony
-    // applications, to certain directories (e.g. cache, log).
-    ->set('writable_mode', 'chmod')
+    // applications, to certain directories (e.g. cache, log). Default value is 'acl'.
+    // Other possible values are: 'chown', 'chgrp' and 'chmod'.
+    // Uncomment if there is an error in making directories writable, in the
+    // deployment process.
+    // ->set('writable_mode', 'chmod')
 ;
 
 
